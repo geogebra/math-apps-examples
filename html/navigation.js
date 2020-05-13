160e22df2be12d2dd7b2fab804457c7b62b7791d
AliciaHof <alicia.hofstaetter@geogebra.org>
2020-05-13 18:32:21 +0200

    new design
    new design, new pages

--- html/navigation.js
+++ html/navigation.js
@@ -2,6 +2,8 @@ function addGithubLink(){
 	var filename = window.location.pathname.split("/").slice(-1)[0];
 var menu =
 '<a href="#" id=drawerToggle class="material-icons">menu</a>'+
+'<img alt="GeoGebra" src="logo.svg" class="headerLogo" draggable="false">'+
+'<h1 class="siteName">Integration Examples</h1>'+
 '<div id=githubLink><a target="_blank" href="https://github.com/geogebra/math-apps-examples/blob/master/html/'+filename+'">View on GitHub</a></span>';
 var menuDiv = document.createElement('div');
 menuDiv.className = "sample-toolbar";
@@ -13,24 +15,22 @@ document.body.insertBefore(menuDiv,document.body.children[0]);
 function addDrawer(){
 	var filename = window.location.pathname.split("/").slice(-1)[0];
 	var menu =
-	'<h2>GeoGebra Math Apps</h2><h4>Examples</h4><ul><li> <a href="https://dev.geogebra.org/examples/html/example-iframe.html">Single applet: iframe</a></li>'+
-	'<li> <a href="https://dev.geogebra.org/examples/html/example-single.html">Single applet</a></li>'+
-	'<li> <a href="https://dev.geogebra.org/examples/html/example-3d.html">Single applet 3D</a></li>'+
-	'<li> <a href="https://dev.geogebra.org/examples/html/example-multiple.html">Two applets</a></li>'+
-	'<li> <a href="https://dev.geogebra.org/examples/html/example-api.html">Single applet + API</a></li>'+
-	'<li> <a href="https://dev.geogebra.org/examples/html/example-api-multiple.html">Two applets + API</a></li>'+
-	'<li> <a href="https://dev.geogebra.org/examples/html/example-api-save-state.html">Single applet + API for saving</a></li>'+
-    '<li> <a href="https://dev.geogebra.org/examples/html/example-graphing.html">Graphing Calculator with Toolbar</a></li>'+
-    '<li> <a href="https://dev.geogebra.org/examples/html/example-geometry.html">Geometry with Toolbar</a></li>'+
-    '<li> <a href="https://dev.geogebra.org/examples/html/example-tools.html">GeoGebra Classic with Toolbar</a></li>'+
-    '<li> <a href="https://dev.geogebra.org/examples/html/example-popup-graphing.html">Graphing Calculator in a Popup Dialog</a></li>'+
-    '<li> <a href="https://dev.geogebra.org/examples/html/example-popup-geometry.html">Geometry in a Popup Dialog</a></li>'+
-    '<li> <a href="https://dev.geogebra.org/examples/html/example-popup-3d.html">3D Graphing Calculator in a Popup Dialog</a></li>'+
-    '<li> <a href="https://dev.geogebra.org/examples/html/example-popup.html">GeoGebra Classic in a Popup Dialog</a></li>'+
-	'<li> <a href="https://dev.geogebra.org/examples/html/example-api-listeners.html">Event listeners</a></li>'+
-	'<li> <a href="https://dev.geogebra.org/examples/html/example-api-sync.html">Communication between applets</a></li>'+
-	'</ul>'+
-	'<h4>Documentation</h4><ul><li><a href="https://www.geogebra.org/manual/en/Reference:Math_Apps_Embedding">Math Apps Embedding</a></li>'+
+	'<h4>Basic</h4>'+
+	'<ul><li> <a href="basic-dynamic-resources.html">Dynamic Resources</a></li>'+
+	'<li> <a href="basic-embedding-options.html">Embedding Options</a></li>'+
+	'<li> <a href="https://dev.geogebra.org/examples/html/example-multiple.html">Hosting Options</a></li></ul>'+
+	'<h4>Plus</h4>'+
+	'<ul><li> <a href="https://dev.geogebra.org/examples/html/example-graphing.html">Dynamic Resources</a></li>'+
+	'<li> <a href="https://dev.geogebra.org/examples/html/example-api.html">Apps Integration</a></li>'+
+	'<li> <a href="https://dev.geogebra.org/examples/html/example-api-multiple.html">Apps in Popups</a></li></ul>'+
+	'<h4>API Integration</h4>'+
+    '<ul><li> <a href="https://dev.geogebra.org/examples/html/example-popup-graphing.html">Saving & Loading</a></li>'+
+    '<li> <a href="https://dev.geogebra.org/examples/html/example-popup-geometry.html">Buttons</a></li>'+
+    '<li> <a href="https://dev.geogebra.org/examples/html/example-popup-3d.html">Event Listeners</a></li>'+
+    '<li> <a href="https://dev.geogebra.org/examples/html/example-popup.html">Communication between Resources</a></li>'+
+	'<li> <a href="https://dev.geogebra.org/examples/html/example-api-listeners.html">LTI Integration</a></li></ul>'+
+	'<h4>Documentation</h4>'+
+	'<ul><li><a href="https://www.geogebra.org/manual/en/Reference:Math_Apps_Embedding">Math Apps Embedding</a></li>'+
 	'<li><a href="https://www.geogebra.org/manual/en/Reference:JavaScript">JavaScript API</a></li></ul>';
 
 	var menuDiv = document.createElement('div');
